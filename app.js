4aa51e56f7b37f4f99c479a5190649a55dda7384
xatkx <xatkx1@gmail.com>
2025-03-18 19:53:45 -0400

    24 de 84 pc

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-
+let my_ataque = "";
+let enemigo_ataque = "";
+let typesAtk= []
 document.addEventListener("DOMContentLoaded", function() {
     console.log("DOM fully loaded and parsed");
 
@@ -6,23 +8,53 @@ document.addEventListener("DOMContentLoaded", function() {
     let btnSelect = document.querySelector("#btn-selecion");
     btnSelect.addEventListener("click", handleButtonClick);
 
-});    
+    let btnfight = document.querySelectorAll("button[name=btn-atk]");
+
+    for(let i = 0; i < btnfight.length; i=i+1)
+    {
+        btnfight[i].addEventListener("click" , atk);
+        typesAtk[i] = btnfight[i].value
+    }
+
+});    // fin de la carga del DOM
 
 function handleButtonClick(event) {
 
 
-    let list = event.target.parentNode.querySelectorAll("input[name=mascota]");
-
-    for( let i = 0; i < list.length; i=i+1)
+    let list = event.target.parentElement.querySelectorAll("input[name=mascota]");
+    let myPet = document.querySelector ("#my-pet");
+    let hisPet = document.querySelector ("#his-pet");
+    let selected = false;
+    for( let i = 0; i < list.length; i=i+1) 
+        // recorro la lista de mascotas y ver si se seleciono una y selecionar una a su vez aleatoria para el enemigo
         {
             if(list[i].checked)
             {
-                console.log(list[i].value);
+                // console.log(`selecionaste a ${list[i].value}`);
+                // console.log(myPet);
+                myPet.textContent = list[i].value;
+                hisPet.textContent = list[aleatorio(0, list.length-1)].value;
+                selected = true;
+
                 break;
             }
         }
-    // console.log("Button was clicked!");
+    if(!selected)
+    // si no se seleciono ninguna mascota
+    {             
+        console.log("No seleccionaste ninguna mascota");
+    }
+
+    // console.log(event.target.parentElement,event.target.parentNode);
 
 };
+function atk(event){
+    my_ataque = event.target.value;
+    // console.log(`atacaste con ${my_ataque}`);
+    enemigo_ataque = enemigoAtk();
+}
 
-// document.parentNode
+function enemigoAtk(){
+    return typesAtk[aleatorio(0,typesAtk.length-1)];
+}
+let aleatorio = (min,max) => Math.floor(Math.random() * (max - min + 1)) + min;
